003885700827e399f3e1f1522e56fcd390433aec
Chuyang Deng <chuyang@seas.upenn.edu>
2017-05-04 15:57:21 -0400

    Added gui class

---
+++ src/gomokugame/InteractiveBoard.java
@@ -0,0 +1,17 @@
+package gomokugame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.ChoiceDialog;
+
+public class InteractiveBoard extends Application {
+	
+	private final Controller controller = new Controller();
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
